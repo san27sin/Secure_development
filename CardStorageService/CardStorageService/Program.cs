309342eef3e936aafc8991deb006bd4476a2dda3
alex <sin27san@gmail.com>
2023-03-09 17:22:58 +0300

    Домашнаяя работа #3, аутентификация, токены, доступные и не доступные методы.

--- CardStorageService/CardStorageService/Program.cs
+++ CardStorageService/CardStorageService/Program.cs
@@ -1,11 +1,16 @@
-
+﻿
 using CardStorageService.Data;
 using CardStorageService.Mapping;
 using CardStorageService.Services;
 using CardStorageService.Services.Impl;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using NLog.Web;
+using System.Numerics;
+using System.Text;
 
 namespace CardStorageService
 {
@@ -49,13 +54,66 @@ namespace CardStorageService
             builder.Services.AddScoped<IClientRepositoryService, ClientRepository>();
             #endregion
 
+            #region Configure Services
+            //регистрируем сервисы общего назначения
+            builder.Services.AddSingleton<IAuthenticateService, AuthenticateService>();
+            #endregion
+
+            #region Configure JWT Tokens
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AuthenticateService.SecretKey)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+            #endregion
+
             builder.Services.AddAutoMapper(typeof(AppMappingProfile));
 
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            //свагер можно конфигурировать
+            builder.Services.AddSwaggerGen( c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CardStorageService", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Description = "JWT Authorization header using the Bearer scheme(Example: 'Bearer 12345abcdef')",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
 
             var app = builder.Build();
 
@@ -64,9 +122,13 @@ namespace CardStorageService
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            } 
+            }
 
-            app.UseAuthorization();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();            
+
             app.UseHttpLogging();
 
             app.MapControllers();
