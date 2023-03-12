e30e97fddf3c643264c7b303f87a2b7d431d449a
alex <sin27san@gmail.com>
2023-03-12 16:18:52 +0300

    Хранение настроек и паролей в файле настроек

---
+++ DataCachingFramework/CacheProvider.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Security.Cryptography;
+
+namespace DataCachingFramework
+{
+    public class CacheProvider
+    {
+        static byte[] additionalEntropy = { 5, 3, 7, 1, 5 };//как соль для шифрования 
+
+        public void CacheConnections(List<ConnectionString> connections)
+        {
+            try
+            {
+                var xmlSerializer = new XmlSerializer(typeof(List<ConnectionString>));
+
+                var memoryStream = new MemoryStream();//для хранения сериолизованных данных в памяти текущего приложения
+                                                      //MemoryStream - класс который позволяет хранить поток данных в памяти текущего приложения 
+
+                var xmlWriter = new XmlTextWriter(memoryStream, Encoding.UTF8);//промежуточный вспомогательный объект который 
+                                                                               //который связан с memorystream и позволяет в этот поток записывать некоторый xml документ
+
+                xmlSerializer.Serialize(xmlWriter, connections);//связываем сериализатор с xmlwriter и с коннектом к БД
+                byte[] protectedData = Protect(memoryStream.ToArray());
+                File.WriteAllBytes($"{AppDomain.CurrentDomain.BaseDirectory}data.protected", protectedData);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Serialize data error.");
+            }
+        }
+
+        public List<ConnectionString> GetConnectionFromCache()
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<ConnectionString>));
+                byte[] data = File.ReadAllBytes($"{AppDomain.CurrentDomain.BaseDirectory}data.protected");
+                return (List<ConnectionString>)xmlSerializer.Deserialize(new MemoryStream(Unprotect(data)));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Deserialize data error.");
+                return null;
+            }            
+        }
+
+        private byte[] Protect(byte[] data)
+        {
+            try
+            {
+                return ProtectedData.Protect(data, additionalEntropy, DataProtectionScope.LocalMachine);
+            }
+            catch(CryptographicException e)
+            {
+                Console.WriteLine($"Protect error.\n{e.Message}");
+                return null;
+            }            
+        }
+
+        private byte[] Unprotect(byte[] data)
+        {
+            try
+            {
+                return ProtectedData.Unprotect(data, additionalEntropy, DataProtectionScope.LocalMachine);
+            }
+            catch(CryptographicException e) 
+            {
+                Console.WriteLine($"Unprotect error.\n{e.Message}");
+                return null;
+            }            
+        }
+    }
+}
---
+++ DataCachingFramework/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCachingFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            Console.Title = Properties.Settings.Default.ApplicationNameDebug;
+#else
+            Console.Title = Properties.Settings.Default.ApplicationName;
+#endif
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.FIO) || Properties.Settings.Default.Age <= 0)
+            {
+                Console.Write("Введите ФИО: ");
+                Properties.Settings.Default.FIO = Console.ReadLine();
+
+                Console.Write("Укажите ваш возраст: ");
+                if (int.TryParse(Console.ReadLine(), out int age))
+                {
+                    Properties.Settings.Default.Age = age;
+                }
+                else
+                {
+                    Properties.Settings.Default.Age = 0;
+                }
+                Properties.Settings.Default.Save(); 
+            }
+
+            Console.WriteLine($"ФИО: {Properties.Settings.Default.FIO}");
+            Console.WriteLine($"Возраст: {Properties.Settings.Default.Age}");
+
+            // %USERPROFILE%\AppData\Local\
+            //зашифровать данные
+            ConnectionString connectionString1 = new ConnectionString
+            {
+                Database = "Database1",
+                Host = "localhost",
+                Password = "12345",
+                UserName = "User1",
+            };
+
+            ConnectionString connectionString2 = new ConnectionString
+            {
+                Database = "Database2",
+                Host = "localhost",
+                Password = "11425",
+                UserName = "User2",
+            };
+
+            List<ConnectionString> connections = new List<ConnectionString>();
+            connections.Add(connectionString1);
+            connections.Add(connectionString2);
+
+            CacheProvider cacheProvider = new CacheProvider();
+            cacheProvider.CacheConnections(connections);
+
+            //расшифровать данные
+            //CacheProvider cacheProvider = new CacheProvider();
+            //List<ConnectionString> connections = cacheProvider.GetConnectionFromCache();
+            
+            //foreach(var connection in connections) 
+            //{
+            //    Console.WriteLine($"{connection.Host} {connection.Database} {connection.UserName} {connection.Password}");
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
