faa412acead0feec24c9c45812f8201032cad1cb
alex <sin27san@gmail.com>
2023-03-13 23:49:38 +0300

    Pre-h/w

---
+++ TwoKeysVerification/CertificateGenerationProvider.cs
@@ -0,0 +1,95 @@
+﻿using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.X509;
+using Org.BouncyCastle.X509.Extension;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoKeysVerification
+{
+    public class CertificateGenerationProvider
+    {
+        public void GenerateRootCertificate(CertificateConfiguration settings)
+        {
+            SecureRandom secRand = new SecureRandom();
+            RsaKeyPairGenerator keyGen = new RsaKeyPairGenerator();
+            RsaKeyGenerationParameters prms = new RsaKeyGenerationParameters(new Org.BouncyCastle.Math.BigInteger("10001", 16), secRand,100, 100);
+            keyGen.Init(prms);
+            AsymmetricCipherKeyPair keyPair = keyGen.GenerateKeyPair();
+
+            string issuer = "CN=" + settings.CertName;
+
+            //Определим имена файлов
+            string p12FileName = settings.OutFolder + @"\" + settings.CertName + ".p12";//сертификат p12 с зыкрытым ключом отдаем разработчику 
+            string crtFileName = settings.OutFolder + @"\" + settings.CertName + ".crt";
+
+            //Серийный номер сертификата
+            byte[] serialNumber = Guid.NewGuid().ToByteArray();
+            serialNumber[0] = (byte)(serialNumber[0] & 0x7f);
+
+            var certGen = new X509V3CertificateGenerator();
+            certGen.SetSerialNumber(new Org.BouncyCastle.Math.BigInteger(1, serialNumber));
+            certGen.SetIssuerDN(new X509Name(issuer));
+            certGen.SetNotBefore(DateTime.Now.ToUniversalTime());
+            certGen.SetNotAfter(DateTime.Now.ToUniversalTime() + new TimeSpan(settings.CertDuration * 365, 0, 0, 0));
+            certGen.SetSubjectDN(new X509Name(issuer));
+            certGen.SetPublicKey(keyPair.Public);
+            certGen.SetSignatureAlgorithm("MD5WITHRSA");
+            certGen.AddExtension(X509Extensions.AuthorityKeyIdentifier, false,
+                new AuthorityKeyIdentifierStructure(keyPair.Public));
+            certGen.AddExtension(X509Extensions.SubjectKeyIdentifier, false,
+                new  SubjectKeyIdentifierStructure(keyPair.Public));
+            certGen.AddExtension(X509Extensions.BasicConstraints, false,
+                new BasicConstraints(true));
+
+            X509Certificate rootCert = certGen.Generate(keyPair.Private);
+
+            //Получим подписанный сертификат
+            byte[] rawCert = rootCert.GetEncoded();
+
+            //Сохраним закрытую часть сертификата
+            try
+            {
+                using (FileStream fs = new FileStream(p12FileName, FileMode.Create))
+                {
+                    Pkcs12Store p12 = new Pkcs12Store();
+                    X509CertificateEntry certEntry = new X509CertificateEntry(rootCert);
+                    p12.SetKeyEntry(settings.CertName, new AsymmetricKeyEntry(keyPair.Private),
+                        new X509CertificateEntry[] { certEntry });
+                    p12.Save(fs, settings.Password.ToCharArray(), secRand);
+                    fs.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                //При сохранении сертификата произошла ошибка
+                throw new CertificateGenerationException("При сохранении закрытой части сертификата произошла ошибка.\r\n" +
+                    ex.Message);
+            }
+
+            //Сохраним открытую часть сертификата
+            try
+            {
+                using (FileStream fs = new FileStream(crtFileName, FileMode.Create))
+                {
+                    fs.Write(rawCert, 0, rawCert.Length);
+                    fs.Close();
+                }
+            }
+            catch (Exception ex) 
+            {
+                //При сохранении сертификата произошла ошибка
+                throw new CertificateGenerationException("При сохранении открытой части сертификата произошла ошибка.\r\n" +
+                    ex.Message);
+            }
+        }
+    }
+}
---
+++ TwoKeysVerification/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoKeysVerification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("~~ Центр генерации сертификатов ~~~\n");
+                Console.WriteLine("1. Создать корневой сертификат");
+                Console.WriteLine("2. Создать сертификат");
+                Console.WriteLine("Выберите подпрограмму (0 - завершение работы приложения): ");
+                if (int.TryParse(Console.ReadLine(), out int no))
+                {
+                    switch (no)
+                    {
+                        case 0:
+                            Console.WriteLine("Завршение работы приложения.");
+                            Console.ReadKey();
+                            return;
+                        case 1:
+                            CertificateConfiguration certificateConfiguration = new CertificateConfiguration
+                            {
+                                CertName = "MyCompany CA",
+                                OutFolder = @"C:\Users\san27sin",
+                                Password = "12345678",
+                                CertDuration = 3
+                            };
+
+                            break;
+                        case 2:
+                            break;
+                        default:
+                            Console.WriteLine("Некорректный номер подпрограммы. Пожалуйста, повторите ввод.");
+                            break;
+                    }
+                }
+                else
+                    Console.WriteLine("Некорректный номер подпрограммы. Пожалуйста, повторите ввод.");
+            }
+        }
+    }
+}
