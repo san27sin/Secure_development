1828755b5977857b22767c0822db15921c05ac04
alex <sin27san@gmail.com>
2023-03-25 14:32:16 +0300

    Домашнее задание #6. Grpc - использование технологии для обмена данных, не используя концепции Rest.

---
+++ CardStorageService/CardStorageClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Net.Client;//создает клиента который позволяет делать клиента по протоколу
+using Microsoft.Data.SqlClient;
+using static ClientServiceProtos.ClientService;
+
+namespace CardStorageClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Создать клиента ...");
+            Console.ReadLine();
+
+            using (var channel = GrpcChannel.ForAddress("http://localhost:5015/"))//создаем один канал
+            {
+                //может быть множество клиентов
+                var client = new ClientServiceClient(channel);
+
+                var response = client.Create(new ClientServiceProtos.CreateClientRequest
+                {
+                    FirstName="Александр",
+                    Surname="Синицын",
+                    Patronymic="Романович"
+                });
+
+                Console.WriteLine($"ClientId: {response.ClientId}; ErrCode: {response.ErrorCode}; ErrMessage: {response.ErrorMessage}");
+            }
+        }
+    }
+}
